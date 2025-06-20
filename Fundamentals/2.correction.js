3050c4495bbdf59be88941ae46a17f8872689532
Collop1 <collop111cop@gmail.com>
2025-06-21 10:00:59 +1200

    Add question 2

---
+++ Fundamentals/2.correction.js
@@ -0,0 +1,12 @@
+let three = "3" 
+let four = "4" 
+let thirty = "30" 
+
+//what is the value of the following expressions? 
+let addition = three + four 
+let multiplication = three * four 
+let division = three / four 
+let subtraction = three - four 
+
+let lessThan1 = three < four 
+let lessThan2 = thirty < four // This is returning true because we are using strings and strings use the first letter to determine if it's bigger or smaller. We can use numbers to resolve this
