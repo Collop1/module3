3a4fd2fbff1b4f631950fe2b022cce9d70ea41fe
Collop1 <collop111cop@gmail.com>
2025-06-21 10:12:35 +1200

    Add question 3

---
+++ Fundamentals/3.whatPrints.js
@@ -0,0 +1,9 @@
+if (0) console.log('#1 zero is true') // 0 is false from binary 0 = false, 1 = true
+if ("0") console.log('#2 zero is true') 
+if (null) console.log('null is true') // null is false cause it doesn't have a value
+if (-1) console.log('negative is true') 
+if (1) console.log('positive is true') 
+if (5) console.log('5 is true') 
+if ("asdf") console.log('string is true') 
+if (undefined) console.log('undefined is true') // undefined is false similar to null cause it doesn't have a value
+if ("") console.log('empty string is true') // empty is false like null
